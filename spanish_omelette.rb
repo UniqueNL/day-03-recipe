4f3c14aa2b75da2475b6511d682abbe2fb537607
UniqueNL <samtaal@outlook.com>
2016-10-04 14:58:27 +0200

    Print out the steps

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -62,3 +62,7 @@ steps = [
   { description: "When almost set, invert on a plate and slide back into the pan", action: "generic_recipe_step" },
   { description: "Cook a few more minutes and slide on to a plate and cool for 10 minutes before serving", action: "generic_recipe_step" }
 ]
+
+steps.each_with_index do |step, index|
+  puts (index + 1).to_s + ") "
+end
